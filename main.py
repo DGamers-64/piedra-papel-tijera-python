d94ba50fadd64106f5805d8cca614b642ec64e09
xLune64 <dgamers.643@gmail.com>
2024-10-28 15:09:00 +0100

    Añadido inicializador del juego, vidas y puntuacion

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import random
 
+puntuacion = 0
+vidas = 3
+
+def inicializarJuego():
+    print("--------------------------------------------------")
+    print(" BIENVENIDO AL JUEGO DE LAS TABLAS DE MULTIPLICAR")
+    print(" VIDAS RESTANTES: ", vidas,)
+    print("--------------------------------------------------")
+
 def preguntar_aleatorio():
     primer_operando = random.randint(0,10)
     segundo_operando = random.randint(0,10)
-    print("PREGUNTA: ", primer_operando, "x", segundo_operando, sep="")
-    respuesta = int(input("Respuesta: "))
-    return primer_operando, segundo_operando, respuesta
+    return primer_operando, segundo_operando
 
-def solucionar(primer_operando, segundo_operando, resultado):
-    if (primer_operando * segundo_operando == resultado):
-        return "CORRECTO!!!"
-    return "INCORRECTO!!!"
+def solucionar(primer_operando, segundo_operando, respuesta):
+    if (primer_operando * segundo_operando == respuesta):
+        return 'ganador'
+    return 'perdedor'
 
-p, s, r = preguntar_aleatorio()
-print(solucionar(p, s, r))
+def ganador(puntuacion):
+    print("¡Correcto!")
+    puntuacion += 1
+    print(puntuacion)
+    return
+
+def perdedor(vidas):
+    print("¡Incorrecto")
+    vidas -= 1
+    print(vidas)
+
+# INICIO PROGRAMA
+
+inicializarJuego()
+primer_operando, segundo_operando = preguntar_aleatorio()
+print("PREGUNTA: ", primer_operando, "x", segundo_operando, sep="")
+respuesta = int(input("Respuesta: "))
+solucion = solucionar(primer_operando, segundo_operando, respuesta)
+print(solucion)
