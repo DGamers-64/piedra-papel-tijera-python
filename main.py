28106c0292bf6c670fb40d7ba70f33bca8976551
xLune64 <dgamers.643@gmail.com>
2024-10-30 11:18:18 +0100

    Dificultad intermedia añadida

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import random
 
-puntuacion = 0
-vidas = 3
-
 def inicializarJuego():
     print("--------------------------------------------------")
     print(" BIENVENIDO AL JUEGO DE LAS TABLAS DE MULTIPLICAR")
     print(" VIDAS DISPONIBLES: ", vidas,)
     print("--------------------------------------------------")
-
+    print(" ELIGE UNA DIFICULTAD:")
+    print("     1. Fácil")
+    print("     2. Intermedio")
+    print("     3. Difícil")
+    dificultad = int(input(" Introduce un número: "))
+    print("--------------------------------------------------")
+    return dificultad
+    
 def preguntar_aleatorio():
     primer_operando = random.randint(0,10)
     segundo_operando = random.randint(0,10)
     return primer_operando, segundo_operando
 
-def solucionar(primer_operando, segundo_operando, respuesta):
-    if (primer_operando * segundo_operando == respuesta):
-        return 'ganador'
-    return 'perdedor'
+def solucionar(primer_operando, segundo_operando, operando, respuesta):
+    match operando:
+        case 1:
+            return primer_operando + segundo_operando == respuesta
+        case 2:
+            return primer_operando - segundo_operando == respuesta
+        case 3:
+            return primer_operando * segundo_operando == respuesta
+        case _:
+            return False
 
 def ganador(puntuacion):
     print("\n¡Correcto!")
@@ -29,19 +39,78 @@ def perdedor(vidas):
     vidas -= 1
     return vidas
 
+def facil(puntuacion, vidas):
+    while(vidas != 0):
+        primer_operando, segundo_operando = preguntar_aleatorio()
+        print("PREGUNTA: ", primer_operando, "x", segundo_operando, sep="")
+        respuesta = int(input("Respuesta: "))
+        solucion = solucionar(primer_operando, segundo_operando, 3, respuesta)
+        if (solucion):
+            puntuacion = ganador(puntuacion)
+        else:
+            vidas = perdedor(vidas)
+        print("\nPuntuación:", puntuacion, "\nVidas restantes:", vidas, "\n")
+    return puntuacion
+
+def intermedio(puntuacion, vidas):
+    while(vidas != 0):
+        operando = random.randint(1,3)
+        match operando:
+            case 1:
+                simbolo_operando = '+'
+            case 2:
+                simbolo_operando = '-'
+            case 3:
+                simbolo_operando = 'x'
+        primer_operando, segundo_operando = preguntar_aleatorio()
+        print("PREGUNTA: ", primer_operando, simbolo_operando, segundo_operando, sep="")
+        respuesta = int(input("Respuesta: "))
+        solucion = solucionar(primer_operando, segundo_operando, operando, respuesta)
+        if (solucion):
+            puntuacion = ganador(puntuacion)
+        else:
+            vidas = perdedor(vidas)
+        print("\nPuntuación:", puntuacion, "\nVidas restantes:", vidas, "\n")
+    return puntuacion
+
+# def dificil(puntuacion, vidas):
+
+
+    # while(vidas != 0):
+    #     operando = random.randint(1,3)
+    #     match operando:
+    #         case 1:
+    #             simbolo_operando = '+'
+    #         case 2:
+    #             simbolo_operando = '-'
+    #         case 3:
+    #             simbolo_operando = '*'
+    #     primer_operando, segundo_operando = preguntar_aleatorio()
+    #     print("PREGUNTA: ", primer_operando, simbolo_operando, segundo_operando, sep="")
+    #     respuesta = int(input("Respuesta: "))
+    #     solucion = solucionar(primer_operando, segundo_operando, operando, respuesta)
+    #     if (solucion):
+    #         puntuacion = ganador(puntuacion)
+    #     else:
+    #         vidas = perdedor(vidas)
+    #     print("\nPuntuación:", puntuacion, "\nVidas restantes:", vidas, "\n")
+    # return puntuacion
+
 # INICIO PROGRAMA
 
-inicializarJuego()
-while(vidas != 0):
-    primer_operando, segundo_operando = preguntar_aleatorio()
-    print("PREGUNTA: ", primer_operando, "x", segundo_operando, sep="")
-    respuesta = int(input("Respuesta: "))
-    solucion = solucionar(primer_operando, segundo_operando, respuesta)
-    if (solucion == 'ganador'):
-        puntuacion = ganador(puntuacion)
-    else:
-        vidas = perdedor(vidas)
-    print("\nPuntuación:", puntuacion, "\nVidas restantes:", vidas, "\n")
+puntuacion = 0
+vidas = 3
+dificultad = inicializarJuego()
+match dificultad:
+    case 1:
+        puntuacion = facil(puntuacion, vidas)
+    case 2:
+        puntuacion = intermedio(puntuacion, vidas)
+    # case 3:
+    #     puntuacion = dificil(puntuacion, vidas)
+    case _:
+        print("Dificultad NO válida:")
+        dificultad = inicializarJuego()
 
 print("RESULTADO FINAL:", puntuacion)
 print("¡Gracias por jugar!")
